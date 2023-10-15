c570215a811b6810bdb51d8ac0b85f6e0dec1e89
Artem Kulikov <chigar2010@yandex.ru>
2023-10-15 21:54:31 +0200

    add setUp()

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,37 +1,32 @@
-# test_calculator.py
 import unittest
 
-# Импортируем класс MadCalculator.
 from calc_code.calculator import MadCalculator
 
 
 class TestCalc(unittest.TestCase):
     """Тестируем MadCalculator."""
 
+    def setUp(self):
+        """Подготовка прогона теста. Вызывается перед каждым тестом."""
+        # Arrange - подготавливаем данные для каждого теста.
+        self.calc = MadCalculator()
+
     def test_sum_string(self):
         """Тестирование функции sum_string с конкатенацией строк."""
-        # Создаём экземпляр класса MadCalculator.
-        calc = MadCalculator()
-        # Вызываем метод.
-        act = calc.sum_string(1, 100500)
-        # Сравниваем фактический результат с ожидаемым.
+        # Убираем создание calc, меняем calc на self.calc.
+        act = self.calc.sum_string(1, 100500)
         self.assertEqual(
             act, 1100500, 'Метод sum_string работает неправильно.'
         )
 
     def test_sum_string_negative_value(self):
         """Тестирование исключения с отрицательным числом."""
-        # Создаём другой экземпляр класса MadCalculator.
-        calc = MadCalculator()
-        # Проверяем, что тест выдаст ошибку ValueError.
         with self.assertRaises(ValueError):
-            # Вызываем метод.
-            calc.sum_string(1, -100500)
+            # Убираем создание calc, меняем calc на self.calc.
+            self.calc.sum_string(1, -100500)
 
     def test_sum_args(self):
         """Тестирование функции суммирования аргументов."""
-        # Создаём ещё один экземпляр класса MadCalculator.
-        calc = MadCalculator()
-        # Вызываем метод.
-        act = calc.sum_args(3, -3, 5)
+        # Убираем создание calc, меняем calc на self.calc.
+        act = self.calc.sum_args(3, -3, 5)
         self.assertEqual(act, 5, 'Метод sum_args работает неправильно.')
